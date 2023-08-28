056a4e35aebcb106e85cb18b06e56c8f67339007
daninjaa <141966059+daninjaa@users.noreply.github.com>
2023-08-27 22:13:36 -0500

    taller 3 Python

---
+++ marca.py
@@ -0,0 +1,12 @@
+class Marca:
+    def __init__(self, nombre):
+        self.__nombre = nombre
+    
+    @property
+    def nombre(self):
+        return self.__nombre
+
+    @nombre.setter
+    def nombre(self, nombre):
+        self.__nombre = nombre
+        
